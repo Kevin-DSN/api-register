28fb855a21644c4725686d3141247d319b95ea6b
Kevin Sosa <dvid_ksn@hotmail.com>
2025-08-14 01:50:00 -0600

    Forzar SSL para Render

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 const { Pool } = require('pg');
 
-const isProduction = process.env.DATABASE_URL && process.env.NODE_ENV === 'production';
-
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || "postgresql://base_postgre_1b2i_user:th8mtMtuyzIqu7AiR86ojYSFtRBzNiu9@dpg-d2eocpbuibrs73884pa0-a.ohio-postgres.render.com/base_postgre_1b2i",
-    ssl: isProduction ? { rejectUnauthorized: false } : false
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
