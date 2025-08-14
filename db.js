76962c2a96b52b30312cdef07c66ba04d5a1fe1e
Kevin Sosa <dvid_ksn@hotmail.com>
2025-08-14 02:24:21 -0600

    Ultimo intento del dia

--- db.js
+++ db.js
@@ -3,13 +3,10 @@ const { Pool } = require('pg');
 const isProduction = !!process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgresql://base_postgre_1b2i_user:th8mtMtuyzIqu7AiR86ojYSFtRBzNiu9@dpg-d2eocpbuibrs73884pa0-a.ohio-postgres.render.com/base_postgre_1b2i",
-  ssl: isProduction
-    ? { rejectUnauthorized: false }  // Render requiere SSL
-    : false                           // Local sin SSL
+  connectionString: process.env.DATABASE_URL || "postgresql://USUARIO:CONTRASEÑA@HOST/NOMBRE_DB",
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
-// Probar la conexión
 pool.connect()
   .then(client => {
     console.log("Conectado a PostgreSQL");
