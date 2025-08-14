4a1d94b0e08961ca8af73c9cada9c4d6f5f116ac
Kevin Sosa <dvid_ksn@hotmail.com>
2025-08-14 01:15:22 -0600

    Descripción breve del cambio

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || "postgres://postgres:tu_contraseña@localhost:5432/registrodb",
+    connectionString: process.env.DATABASE_URL || "postgresql://base_postgre_1b2i_user:th8mtMtuyzIqu7AiR86ojYSFtRBzNiu9@dpg-d2eocpbuibrs73884pa0-a.ohio-postgres.render.com/base_postgre_1b2i",
     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
