f91df1923ebe9d57030d885a6155444996d00e69
Kevin Sosa <dvid_ksn@hotmail.com>
2025-08-14 02:04:07 -0600

    Forzar SSL

--- db.js
+++ db.js
@@ -1,8 +1,4 @@
-const { Pool } = require('pg');
-
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || "postgresql://base_postgre_1b2i_user:th8mtMtuyzIqu7AiR86ojYSFtRBzNiu9@dpg-d2eocpbuibrs73884pa0-a.ohio-postgres.render.com/base_postgre_1b2i",
-    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+    ssl: { rejectUnauthorized: false }
 });
-
-module.exports = pool;
